6be6136c1b2fb0210dbb72217b766e6f588e4607
Emmanuel <Emmydev>
2024-02-09 19:17:45 +0100

    added some new features

--- subletapplication/src/App.jsx
+++ subletapplication/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./component/Navbar";
 import Heropage from "./component/Heropage";
+import Herophoto from "./component/Herophoto";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Heropage/>
+      <Heropage />
+      <Herophoto />
     </>
   );
 };
---
+++ subletapplication/src/component/Herophoto.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import heropicture from "./Herophoto.module.css";
+const Herophoto = () => {
+  return (
+    <section className={heropicture.hero_section}>
+      <main className={heropicture.maincontainer}>
+        <div className={heropicture.imgcontainer1}>
+          <div className={heropicture.dotted}>
+            <div className={heropicture.image1laptop}></div>
+          </div>
+        </div>
+
+        <div className={heropicture.imgcontainer2}>
+          <div className={heropicture.image2youngman}></div>
+        </div>
+
+        <div className={heropicture.imgcontainer3}>
+          <div className={heropicture.dotted2}>
+            <div className={heropicture.image3sittinggirl}></div>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default Herophoto;
