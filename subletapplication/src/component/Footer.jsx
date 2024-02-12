53919da7ee1a90090638008c6b91387ce8da5042
Emmanuel <Emmydev>
2024-02-12 09:46:04 +0100

    added some footer

--- subletapplication/src/component/Footer.jsx
+++ subletapplication/src/component/Footer.jsx
@@ -3,7 +3,36 @@ import footerstyle from "./footer.module.css";
 const Footer = () => {
   return (
     <footer>
-      <div className={footerstyle.footercontainer}></div>
+      <div className={footerstyle.footercontainer}>
+        <div className={footerstyle.flexcontainer}>
+          <div>
+            <h1>logo</h1>
+          </div>
+          <div>
+            <h2>contact us</h2>
+            <ul>
+              <li>08065592378,090575757575</li>
+              <li>088888888888888888</li>
+            </ul>
+          </div>
+          <div>
+            <h2>quick link</h2>
+            <nav>
+              <ul>
+                <li>contact us</li>
+                <li>privacy policy</li>
+                <li>terms of use</li>
+              </ul>
+            </nav>
+          </div>
+          <div>
+            <ul>
+              <li>08065592378,090575757575</li>
+              <li>088888888888888888</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
