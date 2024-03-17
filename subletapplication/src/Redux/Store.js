cdda3c97301e6e88e0b1e6f7615d53d10f9166be
Emmanuel <Emmydev>
2024-03-17 14:54:48 +0100

    added some feature

---
+++ subletapplication/src/Redux/Store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import counterReducer from "../Redux/slice/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    // phone_accessories: counterReducer,
+  },
+});
---
+++ subletapplication/src/Redux/slice/ProfileDashboardSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const counterSlice = createSlice({
+  name: "Phone_accessories",
+  initialState,
+  reducers: {},
+});
+export const {} = counterSlice.actions;
+export default counterSlice.reducer;
